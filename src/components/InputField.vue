3e279ed264f1b4e7bd6449663f9339a94ae35006
Pablo Cid <pablo.cid@wolox.com.ar>
2019-08-05 11:47:11 -0300

    Added signup screen

---
+++ src/components/InputField.vue
@@ -0,0 +1,44 @@
+<template lang="pug">
+  div
+    label.label(
+      v-if='label'
+      :for='name'
+    )
+      | {{ label }}
+    input.input(:id='name' v-model='currentValue' :type='type')
+</template>
+
+<script>
+
+export default {
+  name: 'InputField',
+  props: {
+    name: String,
+    label: String,
+    value: String,
+    type: {
+      type: String,
+      default: 'text'
+    }
+  },
+  data () {
+    return { currentValue: this.value }
+  },
+  watch: {
+    currentValue (newValue) {
+      this.$emit('input', this.currentValue)
+    }
+  }
+}
+
+</script>
+
+<style lang="scss" scoped>
+
+  .label {
+    font-size: 12px;
+    font-weight: 700;
+    padding-left: 5px;
+  }
+
+</style>
---
+++ src/views/Register.vue
@@ -0,0 +1,86 @@
+<template lang="pug">
+  .register
+    .header
+      img.logo(alt='Wolox logo' src='../assets/logo.png')
+      h1.title BOOKS
+    .main
+      form.form(@submit.prevent='onSubmit')
+        input-field.field(v-for='(field, name) in fields' :key='name' :name='name' v-bind='field' v-model='field.value')
+        button.button.submit-button(type="submit") Sign up
+      .separator
+      button.button.secondary-button(type="button") Login
+</template>
+
+<script>
+
+import InputField from '@/components/InputField'
+
+export default {
+  name: 'register',
+  components: {
+    InputField
+  },
+  data () {
+    return {
+      fields: {
+        firstName: { label: 'First name', value: null },
+        lastName: { label: 'Last name', value: null },
+        email: { label: 'Email', value: null },
+        password: { label: 'Password', value: null, type: 'password' }
+      }
+    }
+  },
+  methods: {
+    onSubmit () {
+      console.log(JSON.parse(JSON.stringify(this.fields)))
+    }
+  }
+}
+
+</script>
+
+<style lang="scss" scoped>
+@import '../scss/variables/colors';
+
+  .register {
+    background-color: $grey;
+    border-top: $blue 4px solid;
+    max-width: 600px;
+    padding: 10px;
+  }
+
+  .header {
+    padding: 20px;
+  }
+
+  .title {
+    font-size: 14px;
+    margin: 0;
+  }
+
+  .logo {
+    width: 250px;
+  }
+
+  .main {
+    padding: 0 10px;
+  }
+
+  .separator {
+    background-color: $dark-grey;
+    height: 2px;
+    margin: 20px 0;
+  }
+
+  .field {
+    align-items: flex-start;
+    display: flex;
+    flex-direction: column;
+    padding: 5px 0;
+  }
+
+  .submit-button {
+    margin-top: 15px;
+  }
+
+</style>
